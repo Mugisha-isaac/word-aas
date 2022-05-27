571356373e5beb201ac0b3023ec5709b0cef8d4d
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-05-27 09:03:50 +0200

    feat(api.ts) getRandomWord

--- util/api.ts
+++ util/api.ts
@@ -5,3 +5,15 @@ const getWordFile = async(wordType:string)=>{
        await fetch(process.env.NODE_ENV === 'production' ? "<https://words-aas.vercel.app/db/>": "< http:localhost:3000/db/>" + wordType)
    ).text()
 }
+
+const getRandomWord = (contents:string)=>{
+    console.log("content before: ", contents)
+    contents = contents.replace(/[\\r]/g, "")
+    console.log("contents after: ", contents)
+
+    const words = contents.split("\\n");
+    words.pop();
+
+    const i = Math.floor(Math.random() * words.length);
+    return words[i];
+}
