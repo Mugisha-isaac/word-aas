1567fa9a59a2f4d4ee8fed666fc88ceada7a6a8d
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-05-27 09:43:04 +0200

    wip([slug].ts) adding cors object

--- pages/api/[slug].ts
+++ pages/api/[slug].ts
@@ -1,8 +1,16 @@
 import {NextApiRequest,NextApiResponse} from 'next';
+import NextCors from 'nextjs-cors'
 import {phraseResolver} from '../../util/api';
 
 
 export default async function name(req:NextApiRequest, res:NextApiResponse) {
+ 
+    await NextCors(req,res,{
+        methods:["GET","POST"],
+        origin:"*",
+        optionsSuccessStatus:200
+    });
+
    const {slug} = req.query as {slug:string}
   try{
       const phrase = await phraseResolver(slug);
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import {NextApiRequest,NextApiResponse} from 'next'
-import {phraseResolver} from '../../util/api'
+import {phraseResolver} from '../../util/api';
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
    const {query} = req.body as {query:string}
