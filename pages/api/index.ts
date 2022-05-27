3ecf2bf18a7b6fb0afdf5dbe72e5bf8836f047ae
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-05-27 09:56:00 +0200

    wip(index.ts) done implementations

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,8 +2,16 @@
 
 import {NextApiRequest,NextApiResponse} from 'next'
 import {phraseResolver} from '../../util/api';
+import NextCors from 'nextjs-cors';
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+
+  await NextCors(req,res,{
+    methods:["GET","POST"],
+    origin:"*",
+    optionsSuccessStatus:200
+  })
+
    const {query} = req.body as {query:string}
     try{
          const phrase = await phraseResolver(query);
